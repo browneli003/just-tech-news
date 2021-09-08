2652ae728462e435af9abfd92acaf0e291065b67
Elia Brown <brownelijah143@gmail.com>
2021-09-07 22:41:17 -0600

    if posts if login

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const session = require('express-session');
 const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 
@@ -91,13 +92,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
+  //make sure the session exists first
+  if (req.session) {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote, Comment, User })
+  Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+  }
 });
 
 router.put('/:id', (req, res) => {
